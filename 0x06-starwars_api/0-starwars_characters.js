b94cd26405ea560e824affa605c4b012052b05cd
arkoaikins <arkoaikins@gmail.com>
2024-02-18 21:54:29 -0500

    Write a script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,30 +8,30 @@ if (process.argv.length !== 3) {
 const movieId = process.argv[2];
 
 function fetchCharacters(urlList, index) {
-  if (index >= urlList.length) {
+    if (index >= urlList.length) {
     process.exit(0);
-  }
+}
 
   request(urlList[index], (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      console.log(error);
-      fetchCharacters(urlList, index + 1);
-      return;
+        console.log(error);
+        fetchCharacters(urlList, index + 1);
+        return;
     }
 
-    const character = JSON.parse(body);
-    console.log(character.name);
-    fetchCharacters(urlList, index + 1);
+        const character = JSON.parse(body);
+        console.log(character.name);
+        fetchCharacters(urlList, index + 1);
   });
 }
 
 request(`https://swapi.dev/api/films/${movieId}`, (error, response, body) => {
-  if (error || response.statusCode !== 200) {
+    if (error || response.statusCode !== 200) {
     console.log(error);
     return;
-  }
+}
 
-  const film = JSON.parse(body);
-  const characters = film.characters;
-  fetchCharacters(characters, 0);
+    const film = JSON.parse(body);
+    const characters = film.characters;
+    fetchCharacters(characters, 0);
 });
