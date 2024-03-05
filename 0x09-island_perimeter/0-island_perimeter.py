9d2bdc55fb7811e7963fe58af90444fd97e26563
arkoaikins <arkoaikins@gmail.com>
2024-03-05 15:54:45 -0500

    Create a function def island_perimeter(grid): that returns the perimeter of the island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -22,7 +22,7 @@ def island_perimeter(grid):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
-                
+
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
 
