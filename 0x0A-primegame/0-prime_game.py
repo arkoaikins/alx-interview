d00e1e672450ce89e201eaa6fdff5898cebef588
arkoaikins <arkoaikins@gmail.com>
2024-03-12 12:05:02 -0400

    [task 0]:solve the prime game interview question

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-def isWinner(x, nums):
-    def sieve_of_eratosthenes(n):
-        primes = [True] * (n + 1)
-        p = 2
-        while p * p <= n:
-            if primes[p]:
-                for i in range(p * p, n + 1, p):
-                    primes[i] = False
-            p += 1
-        return [p for p in range(2, n + 1) if primes[p]]
+from sympy import sieve
 
-    def play_game(n):
-        primes = sieve_of_eratosthenes(n)
+"""
+solve the prime game challenge
+"""
+
+
+def isWinner(x, nums, cache={}):
+    """
+      This function determines the winner of a prime number removal game.
+
+    Args:
+        x: The number of rounds played.
+        nums: A list of integers representing the
+        starting set of numbers for each round.
+
+    Returns:
+        A string indicating the winner ("Maria" or "Ben") or
+        None if the winner cannot be determined.
+    """
+    if x in cache:
+        return cache[x]
+
+    winners = []
+
+    for n in nums:
+        primes = list(sieve.primerange(2, n + 1))
         maria_turn = True
 
         while primes:
@@ -29,15 +43,20 @@ def isWinner(x, nums):
                     break
             maria_turn = not maria_turn
 
-        return "Maria" if maria_turn else "Ben"
+        if maria_turn:
+            winners.append("Maria")
+        else:
+            winners.append("Ben")
 
-    wins = [play_game(n) for n in nums]
-    maria_wins = wins.count("Maria")
-    ben_wins = wins.count("Ben")
+    maria_wins = winners.count("Maria")
+    ben_wins = winners.count("Ben")
 
     if maria_wins > ben_wins:
-        return "Maria"
+        result = "Maria"
     elif ben_wins > maria_wins:
-        return "Ben"
+        result = "Ben"
     else:
-        return None
+        result = None
+
+    cache[x] = result
+    return result
