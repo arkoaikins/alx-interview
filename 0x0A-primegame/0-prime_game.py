cccf4b987e12c10816620ba05dc42e07e36b61d8
arkoaikins <arkoaikins@gmail.com>
2024-03-12 12:18:00 -0400

    [task 0]:solve the prime game interview question

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -17,46 +17,54 @@ def isWinner(x, nums):
      A string indicating the winner ("Maria" or "Ben") or None if the winner
      cannot be determined.
     """
+    winners = []
 
-    def sieve_of_eratosthenes(n):
-        """seive the eratosthenes"""
-        primes = [True] * (n + 1)
-        p = 2
-        while p * p <= n:
-            if primes[p]:
-                for i in range(p * p, n + 1, p):
-                    primes[i] = False
-            p += 1
-        return [p for p in range(2, n + 1) if primes[p]]
+    for n in nums:
+        dp = [False] * (n + 1)
+        dp[0] = dp[1] = False
 
-    def play_game(n):
-        primes = sieve_of_eratosthenes(n)
-        maria_turn = True
+        # Precompute a list of primes up to n
+        primes = sieveOfEratosthenes(n)
 
-        while primes:
-            maria_can_move = not primes[0] % 2
-            ben_can_move = not primes[-1] % 2
-            if maria_turn:
-                if maria_can_move:
-                    primes.pop(0)
-                else:
+        # Dynamic programming approach
+        for i in range(2, n + 1):
+            for prime in primes:
+                if prime > i:
                     break
-            else:
-                if ben_can_move:
-                    primes.pop()
-                else:
+                if not dp[i - prime]:
+                    dp[i] = True
                     break
-            maria_turn = not maria_turn
 
-        return "Maria" if maria_turn else "Ben"
+        if dp[n]:
+            winners.append("Maria")
+        else:
+            winners.append("Ben")
 
-    wins = [play_game(n) for n in nums]
-    maria_wins = wins.count("Maria")
-    ben_wins = wins.count("Ben")
+    # Count the number of wins for each player
+    maria_wins = winners.count("Maria")
+    ben_wins = winners.count("Ben")
 
+    # Determine the overall winner
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
+
+
+def sieveOfEratosthenes(n):
+    """
+    Sieve of Eratosthenes to find all prime numbers up to n
+    """
+    primes = []
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+
+    for i in range(2, int(n**0.5) + 1):
+        if sieve[i]:
+            primes.append(i)
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+
+    return primes
